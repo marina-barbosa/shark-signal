0e9d0ad724a64897aa206ae856696052c185bba1
marina-barbosa <marinabarbosa.exp@gmail.com>
2025-03-21 13:17:11 -0300

    feat: add model appUser

---
+++ api/Models/AppUser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace api.Models;
+
+public class AppUser
+{
+    public string? FullName { get; set; }
+    public string? ProfileImage { get; set; }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,16 @@
 using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(x=>x.UseSqlite("Data Source=sharksignal.db"));
+builder.Services.AddDbContext<AppDbContext>(x=>x
+.UseSqlite("Data Source=sharksignal.db"));
+
+builder.Services.AddIdentityCore<AppUser>()
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
