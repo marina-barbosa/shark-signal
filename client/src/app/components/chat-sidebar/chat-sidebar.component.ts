fc779ca7ffcfe5dee0fb5bb87cd8153ef5dc412e
marina-barbosa <marinabarbosa.exp@gmail.com>
2025-03-22 13:24:46 -0300

    wip: chat-sidebar

---
+++ client/src/app/components/chat-sidebar/chat-sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-chat-sidebar',
+  imports: [MatIconModule, MatMenuModule],
+  templateUrl: './chat-sidebar.component.html',
+  styles: ``
+})
+export class ChatSidebarComponent {
+
+}
