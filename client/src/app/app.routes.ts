ea9a643ebbafa528bbbbadff57d350547037735f
marina-barbosa <marinabarbosa.exp@gmail.com>
2025-03-22 12:39:27 -0300

    feat: adicionar rota wildcard para redirecionamento ao chat

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,16 +1,32 @@
 import { Routes } from '@angular/router';
 import { loginGuard } from './guards/login.guard';
+import { authGuard } from './guards/auth.guard';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
+
+    
     { path: 'register',
         loadComponent: () =>
             import('./register/register.component').then((x) => x.RegisterComponent),
-            canActivate: [loginGuard],
+        canActivate: [loginGuard],
     },
     {
         path: 'login',
         loadComponent: () =>
             import('./login/login.component').then((x) => x.LoginComponent),
-            canActivate: [loginGuard],
-    }
+        canActivate: [loginGuard],
+    },
+    {
+        path: 'chat',
+        component: ChatComponent,
+        canActivate: [authGuard],
+    },
+        
+    { 
+        path: '**', 
+        redirectTo: 'chat', 
+        pathMatch: 'full' 
+    }, // posicao importa
+    
 ];
